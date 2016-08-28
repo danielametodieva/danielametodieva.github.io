9ac7b66ad6e096f5d9c58cbc66e06dfbc02a4221
Daniela Metodieva <metodieva.daniel@gmail.com>
2016-08-28 12:15:15 +0300

    fix(Dependencies' banners): Removed with stripCssComments

--- configs/grunt/watch.js
+++ configs/grunt/watch.js
@@ -11,7 +11,7 @@ module.exports = {
 
     styles: {
         files: ['src/styles/**/*.scss', 'src/images/profiles/*.*', 'src/images/toggles/*.*', 'src/model/profiles.json'],
-        tasks: ['scsslint', 'clean:styles', 'compass']
+        tasks: ['scsslint', 'clean:styles', 'stripCssComments', 'compass']
     },
 
     scripts: {
