af45c98c9d27002dfe4c774adefe1101fd7684ed
Daniela Metodieva <metodieva.daniel@gmail.com>
2016-08-29 11:05:26 +0300

    fix(Modernizr): Replaced full version with a custom build

---
+++ configs/grunt/modernizr.js
@@ -0,0 +1,14 @@
+module.exports = {
+    task: {
+        "crawl": false,
+        "customTests": [],
+        "dest": "src/scripts/modernizr-webp.min.js",
+        "tests": [
+            "webp"
+        ],
+        "options": [
+            "setClasses"
+        ],
+        "uglify": true
+    }
+};
